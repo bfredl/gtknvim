86ee850b489e376a9e8ea54adbf7ca1b8556f9a4
Björn Linse <bjorn.linse@gmail.com>
2018-09-23 11:18:13 +0200

    add gtk-rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,33 @@
 //use futures::future::Future; // Note: It's not `futures_preview`
 use futures::executor::block_on;
 
+use gtk::prelude::*;
+use gtk::{Button, Window, WindowType};
+
 fn main() {
-    println!("Hello, world!");
-    block_on(goff())
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("First GTK+ Program");
+    window.set_default_size(350, 70);
+    let button = Button::new_with_label("Click me!");
+    window.add(&button);
+    window.show_all();
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    button.connect_clicked(|_| {
+        println!("Clicked!");
+    });
+
+    block_on(goff());
+    gtk::main();
 }
 
 async fn goff() {
